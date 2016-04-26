42aeab9b0c7621f1c6b2718151418788977b679f
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-26 18:46:38 +0800

    Adds validation to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
 
+  # Validations
+  validates :title, presence: true
+  validates :body, presence: true
 
 end
--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Creating Articles' do
-  scenario 'A user creates a new article' do
+  scenario 'An user creates a new article' do
     visit '/'
 
     click_link 'New Article'
@@ -13,5 +13,18 @@ RSpec.feature 'Creating Articles' do
     expect(page).to have_content('Article has been created')
     expect(page.status_code).to eq 200
     expect(page.current_path).to eq(articles_path)
+  end
+
+  scenario 'An user fails to create a new article' do
+    visit '/'
+
+    click_link 'New Article'
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has not been created')
+    expect(page).to have_content('Title can\'t be blank')
+    expect(page).to have_content('Body can\'t be blank')
   end
 end
