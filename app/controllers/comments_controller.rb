ab22de8efd8a11b5494f30f99cfee0c3339f03f8
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-04-28 21:22:18 +0800

    Implements adding comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 
+  before_action :authenticate_user!
   before_action :set_article
 
   def create
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
+require 'support/macros'
+
 
 RSpec.describe CommentsController, type: :controller do
+
+  describe 'POST #create' do
+
+    before do
+      @jon = User.create!(email: 'jon.snow.2@example.com', password: 'password')
+    end
+
+    context 'Signed in user' do
+      it 'should authorize the user to create a comment' do
+        sign_in @jon
+
+        article = @jon.articles.build(title: 'First article', body: 'First article')
+        article.save
+        post :create, :comment => { body: 'I love Game of Thrones' }, article_id: article.id
+
+        expect(flash[:success]).to eq 'Comment has been added'
+      end
+    end
+
+    context 'Non signed in user' do
+      it 'should redirect to the sign in page' do
+        article = Article.create(title: 'First article', body: 'First article', user: @jon)
+        post :create, :comment => { body: 'I love Game of Thrones' }, article_id: article.id
+
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+  end
 
 end
